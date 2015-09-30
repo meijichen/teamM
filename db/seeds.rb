282c484705d7909530162cdb50d53f327c3a212e
moelgendy <mohamed@elgendyconsulting.com>
2015-09-30 10:07:09 -0400

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Cooler.create [ { name: 'Meiji Chen', github_link: 'https://github.com/meijichen', email: 'm@chen.com' , bio: 'no bio', coolnes: 10},
+                 { name: 'Ed Petner', github_link: 'https://github.com/edpetner', email: 'e@petner.com' , bio: 'no bio', coolnes: 10},
+                 { name: 'Mohamed Elgendy', github_link: 'https://github.com/moelgendy', email: 'm@elgendy.com' , bio: 'no bio', coolnes: 10},
+                 { name: 'Andres Carter', github_link: 'https://github.com/cartermarino', email: 'a@carter.com' , bio: 'no bio', coolnes: 10}]
